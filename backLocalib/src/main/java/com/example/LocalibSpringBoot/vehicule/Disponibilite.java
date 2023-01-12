2dc6f775300d6d84eca77256c810de7d0587d463
Rym Haddouche <rymmihoub@gmail.com>
2023-01-12 09:32:44 +0100

    fix: supression dossier backLocalib

--- backLocalib/src/main/java/com/example/LocalibSpringBoot/vehicule/Disponibilite.java
+++
@@ -1,5 +0,0 @@
-package com.example.LocalibSpringBoot.vehicule;
-
-public enum Disponibilite {
-    Disponible, Indisponible;
-}
--- backLocalib/src/main/java/com/example/LocalibSpringBoot/vehicule/TypeVehicule.java
+++
@@ -1,5 +0,0 @@
-package com.example.LocalibSpringBoot.vehicule;
-
-public enum TypeVehicule {
-    Voiture, Camion, Motocyclette, Fourgon, cyclomoteur;
-}
